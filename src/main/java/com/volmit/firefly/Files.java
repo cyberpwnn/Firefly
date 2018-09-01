2173476fda0802198559e6992bf900f278bc3684
cyberpwn <danielmillst@gmail.com>
2018-09-01 17:47:46 -0400

    files

--- src/main/java/com/volmit/firefly/Files.java
+++ src/main/java/com/volmit/firefly/Files.java
@@ -1,17 +1,100 @@
 package com.volmit.firefly;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 
 import org.apache.commons.io.FileUtils;
+import org.zeroturnaround.zip.ZipUtil;
+
+import com.jcraft.jsch.Channel;
+import com.jcraft.jsch.ChannelSftp;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.Session;
 
 public class Files
 {
 	public String path()
 	{
 		return new File(".").getAbsolutePath();
+	}
+
+	public void archive(String folder, String destination)
+	{
+		archive(folder, destination, 3);
+	}
+
+	public void archive(String folder, String destination, int level)
+	{
+		System.out.println("Archiving " + new File(folder).getPath());
+		ZipUtil.pack(new File(folder), new File(destination), level);
+	}
+
+	public void send(String host, int port, String username, String password, String dir, ArrayList<String> put, String key, String pass)
+	{
+		String SFTPHOST = host;
+		int SFTPPORT = port;
+		String SFTPUSER = username;
+		String SFTPPASS = password;
+		String SFTPWORKINGDIR = dir;
+		Session session = null;
+		Channel channel = null;
+		ChannelSftp channelSftp = null;
+		System.out.println("Prepare the host information for sftp.");
+		try
+		{
+			JSch jsch = new JSch();
+			jsch.addIdentity(key, pass);
+			session = jsch.getSession(SFTPUSER, SFTPHOST, SFTPPORT);
+			session.setPassword(SFTPPASS);
+			java.util.Properties config = new java.util.Properties();
+			config.put("StrictHostKeyChecking", "no");
+			session.setConfig(config);
+			session.connect();
+			System.out.println("Host connected.");
+			channel = session.openChannel("sftp");
+			channel.connect();
+			System.out.println("sftp channel opened and connected.");
+			channelSftp = (ChannelSftp) channel;
+			channelSftp.cd(SFTPWORKINGDIR);
+			for(String i : put)
+			{
+				File f = new File(i);
+
+				if(f.exists() && f.isFile())
+				{
+					System.out.println("Uploading " + f.getName());
+					channelSftp.put(new FileInputStream(f), f.getName());
+					System.out.println("Uploaded " + f.getName());
+				}
+
+				if(i.startsWith("dir:::"))
+				{
+					System.out.println("Make Directory: " + i.split(":::")[1]);
+					channelSftp.mkdir(i.split(":::")[1]);
+					System.out.println("Made Directory: " + i.split(":::")[1]);
+				}
+			}
+		}
+
+		catch(Exception ex)
+		{
+			System.out.println("Exception found while tranfer the response.");
+			ex.printStackTrace();
+		}
+
+		finally
+		{
+			channelSftp.exit();
+			System.out.println("sftp Channel exited.");
+			channel.disconnect();
+			System.out.println("Channel disconnected.");
+			session.disconnect();
+			System.out.println("Host Session disconnected.");
+		}
 	}
 
 	public boolean has(String path)
@@ -83,6 +166,11 @@ public class Files
 
 			f.delete();
 		}
+
+		else
+		{
+			System.out.println("Unable to delete, doesnt exist: " + path);
+		}
 	}
 
 	public void copy(String from, String to)
@@ -92,6 +180,7 @@ public class Files
 
 		if(!fromx.exists())
 		{
+			System.out.println("Doesnt exist for copy " + from);
 			return;
 		}
 
@@ -100,17 +189,17 @@ public class Files
 			if(!tox.exists())
 			{
 				tox.mkdirs();
+			}
 
-				try
-				{
-					System.out.println("Copying directory: " + new File(from).getPath() + " to " + new File(to).getPath());
-					FileUtils.copyDirectory(new File(from), new File(to));
-				}
+			try
+			{
+				System.out.println("Copying directory: " + new File(from).getPath() + " to " + new File(to).getPath());
+				FileUtils.copyDirectory(new File(from), new File(to));
+			}
 
-				catch(IOException e)
-				{
-					e.printStackTrace();
-				}
+			catch(IOException e)
+			{
+				e.printStackTrace();
 			}
 		}
 
@@ -137,6 +226,11 @@ public class Files
 				{
 					e.printStackTrace();
 				}
+			}
+
+			else
+			{
+				System.out.println("Cannot copy to from folder / file " + to);
 			}
 		}
 	}
